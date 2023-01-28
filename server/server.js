05a45333aefe10cef38d1d64a6b5db2170ca6df8
Iman Anooshehpour <imananoosheh@gmail.com>
2023-01-28 13:08:15 -0800

    Parameterized and finished 2 login and verify api calls to server

---
+++ server/server.js
@@ -0,0 +1,69 @@
+const dotenv = require("dotenv");
+dotenv.config();
+
+const express = require("express");
+const jwt = require("jsonwebtoken");
+
+const { sendTokenViaEmail } = require("./mailer");
+
+const app = express();
+app.use(express.urlencoded({ extended: true }));
+
+const USERS = [
+    {
+        id: 1,
+        email: "imananoosheh@gmail.com",
+        name: "Iman",
+    },
+];
+
+app.post("/login", async (request, response) => {
+    // USERS.find() function will be replace with querying from a DB
+    // TODO: connect a database of users
+    const user = USERS.find((u) => u.email === request.body.email);
+
+    if (user != null) {
+        try {
+            const token = jwt.sign(
+                { userId: user.id },
+                process.env.JWT_HASH_SECRET,
+                { expiresIn: "1h" }
+            );
+            await sendTokenViaEmail(user.email, token);
+        } catch (error) {
+            return response.send(
+                "Encounter an error while logging in! Please try again."
+            );
+        }
+    }
+    response.send("Check your email to finish logging in");
+});
+
+app.get("/verify", (request, response) => {
+    const token = request.query.token;
+    if (token == null) return response.sendStatus(401);
+
+    try {
+        const decodedToken = jwt.verify(token, process.env.JWT_HASH_SECRET);
+        const user = USERS.find((u) => u.id === decodedToken.userId);
+        response.send(`Welcome ${user.name}!\nYou are logged in!`);
+    } catch (error) {
+        response.sendStatus(401);
+    }
+});
+
+app.listen(process.env.PORT);
+
+/**
+ * TO GENERATE SECRET CODE FOR JWT
+    crypto.subtle.generateKey({
+    name: "HMAC",
+        hash: {name: "SHA-256"},
+    },true, ["sign", "verify"]).then(key=>{
+    crypto.subtle.exportKey("jwk", key).then(exported => {
+        console.log(exported.k)
+    })
+    })
+ * 
+ * 
+ *  */
