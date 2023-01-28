fe76515dba9ee0d2a9de35f188e8815a92ec9b17
Iman Anooshehpour <imananoosheh@gmail.com>
2023-01-28 13:08:49 -0800

    styled email send to users

--- server/mailer.js
+++ server/mailer.js
@@ -7,7 +7,7 @@ function sendTokenViaEmail(email, token) {
         from: process.env.EMAIL_FROM,
         to: email,
         subject: "Finish Logging in",
-        html: `<div style="display:flex;flex-direction:column;justify-content:center;align-content:center;gap:2rem"><p>Here is you link to click and login</p><a href="http://${process.env.DOMAIN}:${process.env.PORT}/verify?token=${token}">Log In</a></div>`,
+        html: `<div style="display:flex;flex-direction: column;width: 50vmin;font-size: 1.5rem;align-items: center;height: 200px;justify-content: space-evenly;gap:1.5rem"><p>Please use the link below to finish your logging in.</p><a href="http://${process.env.DOMAIN}:${process.env.PORT}/verify?token=${token}">Log In</a></div>`,
     };
     return mailer.send(message);
 }
